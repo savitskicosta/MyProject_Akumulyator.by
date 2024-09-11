6767bd845e06a524f58df17c6688f0f332e253eb
savitskicosta <const.savitski@gmail.com>
2024-09-11 13:01:33 +0300

    searh

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
-$(function(){
-
+$(function () {
+	$('.search__tabs-item').on('click', function (e) {
+		e.preventDefault();
+		$('.search__tabs-item').removeClass('search__tabs-item--active');
+		$('.search__content-item').removeClass('search__content-item--active');
+		$(this).addClass('search__tabs-item--active');
+		$($(this).attr('href')).addClass('search__content-item--active');
+	});
 });
